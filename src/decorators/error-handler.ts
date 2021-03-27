e67d0906f90433f51b0aa9030109ebc22d8e7e08
dmitryi <dmitriykatashinskiywork@gmail.com>
2021-03-27 03:08:30 +0200

    incorrect test for testing husky

--- src/decorators/error-handler.ts
+++ src/decorators/error-handler.ts
@@ -8,7 +8,6 @@ export function ErrorHandler(value: string) {
         descriptor.value.call(this, ...arguments);
       }catch (error) {
         logger.log(value);
-        console.log("", error);
         // tslint:disable-next-line:no-console
         console.error(error);
       }
